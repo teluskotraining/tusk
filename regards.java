592da52f7122b2f0f735c6acc9f7748ec3806970
Sibajee Ray <35264737+sibajeeray@users.noreply.github.com>
2018-01-09 20:46:45 +0530

    File created

---
+++ regards.java
@@ -0,0 +1,10 @@
+class Regards
+{
+  public void main(String[] args)
+  {
+    //Regards
+    System.out.println("Thank you Sir..I'm learning so many things from you.");
+    
+    //Request
+    System.out.println("I will be greatefull if you'll teach something more on Maven..");
+  }
