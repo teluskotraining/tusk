bd7a75deee3830fef3adc80747b06ac5ab137dc8
Vikas Pradhan <40077717+VRedBull@users.noreply.github.com>
2020-10-10 19:15:45 +0530

    A Pizza made with goodness of Java

---
+++ PizzaRecipe.java
@@ -0,0 +1,9 @@
+public class PizzaRecipe{
+  public static void main(String...args){
+    makePizza();
+  }
+  
+  pulic void makePizza(){
+    System.out.print("Search on Google how to make a Pizza :P");
+  }
+}
